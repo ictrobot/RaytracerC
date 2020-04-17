ee0e7fa95a2cff987775118bc51b73e039664a13
ictrobot <ictrobot@outlook.com>
2020-04-17 14:07:47 +0100

    Logging in main

--- src/main.c
+++ src/main.c
@@ -1,8 +1,32 @@
 #include "objects/scene.h"
 #include "renderer.h"
+#include "stdio.h"
+#include <time.h>
+
+const static int width = 1920;
+const static int height = 1080;
 
 int main() {
   Scene *scene = scene_make();
-  Image image = render(1920, 1080, 60, scene, -1);
-  return img_save(image, "test.png");
+  printf("Initialized scene with %i objects and %i lights\n", scene->lenObj, scene->lenLight);
+
+  printf("Starting %ix%i render\n", width, height);
+  clock_t t = clock();
+  Image image = render(width, height, 60, scene, 0);
+  t = clock() - t;
+  double time_taken = ((double) t) / CLOCKS_PER_SEC;
+  printf("Rendered scene in %.3f seconds\n", time_taken);
+
+  t = clock();
+  int returnCode = img_save(image, "render.png");
+
+  if (returnCode == 0) {
+    t = clock() - t;
+    time_taken = ((double) t) / CLOCKS_PER_SEC;
+    printf("Saved PNG in %.3f seconds\n", time_taken);
+  } else {
+    fprintf(stderr, "Failed to save PNG");
+  }
+
+  return returnCode;
 }
